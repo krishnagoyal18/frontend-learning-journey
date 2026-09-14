90c05f4fcd3b1e54a69c462ecf3c444882e156f3
Krishna Goyal <krgoyal1801@gmail.com>
2026-09-14 21:45:19 +0530

    Added loops and strings practice

---
+++ JavaScript/Loops-Strings/day-04.js
@@ -0,0 +1,29 @@
+// let n = 10;
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+//even numbers 
+
+// let n =20;
+// for(let i=1; i<=20; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+//Reverse number
+// let n = 10;
+// for(let i=10; i>=1; i--){
+//     console.log(i);
+// }
+
+
+//string practice 
+
+// str = "Krishna";
+// console.log(str.length);//7
+
+// str = "krishna";
+// console.log(str[0]);//k
+
