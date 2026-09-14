fc7fff1c094f0ff3cd9903108d4a5aa4a96bb547
Krishna Goyal <krgoyal1801@gmail.com>
2026-09-14 22:15:24 +0530

    Added DOM and BOM practice

---
+++ JavaScript/DOM/day-09.js
@@ -0,0 +1,14 @@
+//DOM Manipulation
+//DOM - Document object model - It is a tree like structure
+/*Agar hm kisi html code ko js object mai to yaijs object 
+   document kehlata hai or iss process ko DOM kehte hai */
+// Characters -> tags -> token -> node -> DOM
+//BOM - Browser Object Model
+//html ki file mai krenge
+//browser mai inspect krke console pe document.getElementById("Id name");
+//Isse id ka data fetch ho  jayega 
+//termnologies - 
+//window object - It is a global object 
+//window.open() se hm new window or tab khol sakte hai 
+//window.close() se hm band bhi kr sakte hai 
+//window object is created by your browser
